3a9553810bf87ab87b03759d83d91f6c8e75308b
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-03-15 13:51:24 -0400

    Update rod.c

--- rod.c
+++ rod.c
@@ -35,8 +35,8 @@ s * print_cut(int * p, int n)
     s * ret = bot_up_cut_rod(p, n);
     while(n > 0)
     {
-        printf("%d ", (ret->cut)[n]);
-        n -= (ret->cut)[n];
+        printf("%d ", ret->cut[n]);
+        n -= ret->cut[n];
     }
     printf("\n");
     return ret;
@@ -61,7 +61,7 @@ int main()
     int price[] = {1 << 31, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
     int n = sizeof(price) / sizeof(*price);
     
-    for(int i = 1; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
         printf("len = %d:  ", i);
         print_cut(price, i);
