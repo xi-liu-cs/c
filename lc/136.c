feb47b4290254e1bcce0d5eaa1e99cc90c478f87
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-03-16 10:52:01 -0400

    Update 136.c

--- lc/136.c
+++ lc/136.c
@@ -1,7 +1,7 @@
 int singleNumber(int * nums, int nums_size)
 {
     int ret = 0;
-    for(int i = 0; i < nums_size; i++)
+    for(int i = 0; i < nums_size; ++i)
         ret ^= nums[i];
     return ret;
 }
