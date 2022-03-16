bb23b67e80a30a6a6bc5e0d45d2d78a22b2e2067
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-03-16 12:18:59 -0400

    Update 144.c

--- lc/144.c
+++ lc/144.c
@@ -1,24 +1,25 @@
 typedef struct TreeNode node;
 
-void preorder(node * root, int * ret, int * return_size, int * cur_size)
+int ** preorder(node * root, int ** ret, int * return_size, int * cur_size)
 {
     if(!root)
-        return;
-    if(*return_size + 1 > *cur_size)
+        return ret;
+    if(*return_size >= *cur_size)
     {
-        *cur_size += 100;
-        ret = realloc(ret, *cur_size * sizeof(int));
+        *cur_size = *return_size + 10;
+        *ret = realloc(*ret, *cur_size * sizeof(int));
     }
-    ret[(*return_size)++] = root->val;
+    (*ret)[(*return_size)++] = root->val;
     preorder(root->left, ret, return_size, cur_size);
     preorder(root->right, ret, return_size, cur_size);
+    return ret;
 }
 
 int * preorderTraversal(node * root, int * return_size)
 {
-    int cur_size = 100;
+    int cur_size = 10;
     int * ret = malloc(cur_size * sizeof(int));
     *return_size = 0;
-    preorder(root, ret, return_size, &cur_size);
-    return ret;
+    int ** ret2 = preorder(root, &ret, return_size, &cur_size);
+    return *ret2;
 }
