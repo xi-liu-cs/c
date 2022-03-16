aafedaabde832c5d3499ad784c00faaf5090ad10
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-03-16 16:06:17 -0400

    Create 160.c

---
+++ lc/160.c
@@ -0,0 +1,14 @@
+typedef struct ListNode node;
+
+node * getIntersectionNode(node * head_a, node * head_b)
+{
+    if(!head_a || !head_b)
+        return 0;
+    node * a = head_a, * b = head_b;
+    while(a != b)
+    {
+        a = a ? a->next : head_b;
+        b = b ? b->next : head_a;
+    }
+    return a;
+}
