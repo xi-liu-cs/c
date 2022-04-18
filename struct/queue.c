44d0fd841a6ae127553e9b5d1c5eb42992ef9215
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-04-18 10:34:21 -0400

    Create queue.c

---
+++ struct/queue.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct queue 
+{
+    int head, tail, size;
+    unsigned block, cur_sz;
+    void ** array;
+}queue;
+ 
+queue * create_queue()
+{
+    queue * q = (queue *)malloc(sizeof(queue));
+    q->head = q->size = 0;
+    q->block = q->cur_sz = 32;
+    q->tail = q->cur_sz - 1;
+    q->array = malloc(q->cur_sz * sizeof(void *));
+    return q;
+}
+
+int is_full(queue * q){ return q->size == q->cur_sz; }
+int is_empty(queue * q){ return q->size == 0; }
+
+void enqueue(queue * q, void * item)
+{
+    if (is_full(q))
+    {
+        q->cur_sz += q->block;
+        q->array = realloc(q->array, q->cur_sz * sizeof(void *));
+    }
+    q->tail = (q->tail + 1) % q->cur_sz;
+    q->array[q->tail] = item;
+    ++(q->size);
+    printf("%d enqueued to queue\n", *(int *)item);
+}
+
+void * dequeue(queue * q)
+{
+    if (is_empty(q))
+        return 0;
+    void * item = q->array[q->head];
+    q->head = (q->head + 1) % q->cur_sz;
+    --(q->size);
+    return item;
+}
+
+void * front(queue * q)
+{
+    if (is_empty(q))
+        return 0;
+    return q->array[q->head];
+}
+ 
+void * back(queue * q)
+{
+    if (is_empty(q))
+        return 0;
+    return q->array[q->tail];
+}
+
+int main()
+{
+    queue * q = create_queue();
+    
+    int data[] = {4, 3, 2, 1, 0};
+    enqueue(q, data);
+    enqueue(q, data + 1);
+    enqueue(q, data + 2);
+    enqueue(q, data + 3);
+    
+    printf("%d dequeued from queue\n\n", *(int *)dequeue(q));
+    printf("front item is %d\n", *(int *)front(q));
+    printf("back item is %d\n", *(int *)back(q));
+}
