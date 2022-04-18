5eae9d49c9e7fc800cba1c56758d3982331f2a5a
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-04-18 17:23:48 -0400

    Update 2d pass.c

--- 2d/2d pass.c
+++ 2d/2d pass.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
 
+void p1(int r, int c, int (*a)[c])
+{
+    for(int i = 0; i < r; ++i)
+    {
+        for(int j = 0; j < c; ++j)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+}
+
+void p2(int r, int c, int a[][c])
+{
+    for(int i = 0; i < r; ++i)
+    {
+        for(int j = 0; j < c; ++j)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int r = 3, c = 4, cnt = 0;
+    int a[r][c];
+    for(int i = 0; i < r; ++i)
+        for(int j = 0; j < c; ++j)
+            a[i][j] = cnt++;
+    printf("p1:\n");
+    p1(r, c, a);
+    printf("\np2:\n");
+    p2(r, c, a);
+}
