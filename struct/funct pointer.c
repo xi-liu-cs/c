61f0d36cd08e4207a97839875eda0e828de895eb
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-04-21 21:28:33 -0400

    Update funct pointer.c

--- struct/funct pointer.c
+++ struct/funct pointer.c
@@ -7,16 +7,15 @@ typedef struct node
     void (* print)(struct node *);
 }node;
 
-void print(node * n)
-{
-    printf("node:\n");
-    printf("%d\n", *(int *)n->data);
-}
-
 node * malloc_node(void * data)
 {
     node * n = (node *)malloc(sizeof(node));
     n->data = data;
+    void print(node * n)
+    {
+        printf("node:\n");
+        printf("%d\n", *(int *)n->data);
+    }
     n->print = print;
     return n;
 }
