dd9e5529f0c487526542b5f34a99926b3abe4676
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-01 21:23:38 -0400

    Update reference in for loop.cc

--- general/reference in for loop.cc
+++ general/reference in for loop.cc
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <vector>
+using namespace std;
+
+void print(vector<int> a)
+{
+    for(auto & a_i : a)
+        printf("%d ", a_i);
+    printf("\n");
+}
 
 int main()
 {
-    int a[] = {1, 2, 3};
-    for(auto & a_i: a) /* reference */
+    vector<int> a = {1, 2, 3}; 
+    for(auto & a_i : a) /* reference */
         a_i = 0;
     printf("a:\n");
-    for(auto & a_i: a)
-        printf("%d ", a_i);
-    printf("\n");
+    print(a);
     
-    int a2[] = {1, 2, 3};
-    for(auto a2_i: a2) /* copy */
+    vector<int> a2 = {1, 2, 3};
+    for(auto a2_i : a2) /* copy */
         a2_i = 0;
     printf("a2:\n");
-    for(auto & a2_i: a2)
-        printf("%d ", a2_i);
-    printf("\n");
+    print(a2);
 }
