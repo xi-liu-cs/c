f6752b4c3a53477ea6627b643f4c515f6081c545
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-06 21:03:54 -0400

    Create disjoint set is cycle.c

---
+++ algo/graph/disjoint set is cycle.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct vtex
+{
+    int vtex_id;
+}vtex;
+
+typedef struct edge
+{
+    vtex * src, * dest;
+}edge;
+
+typedef struct graph
+{
+    int n_vtex, n_edge, cur_edge;
+    edge * e_ary;
+}graph;
+
+graph * malloc_graph(int n_vtex, int n_edge)
+{
+    graph * g = (graph *)malloc(sizeof(graph));
+    g->n_vtex = n_vtex;
+    g->n_edge = n_edge;
+    g->cur_edge = 0;
+    edge * e_ary = (edge *)malloc(n_edge * sizeof(edge));
+    g->e_ary = e_ary;
+    return g;
+}
+
+void add_edge(graph * g, vtex * src, vtex * dest)
+{
+    g->e_ary[g->cur_edge].src = src;
+    g->e_ary[g->cur_edge].dest = dest;
+    ++g->cur_edge;
+}
+
+vtex * find(vtex ** parent, vtex * i)
+{
+    if(!parent[i->vtex_id])
+        return i;
+    return find(parent, parent[i->vtex_id]);
+}
+
+void uni(vtex ** parent, vtex * a, vtex * b)
+{
+    a = find(parent, a);
+    b = find(parent, b);
+    parent[a->vtex_id] = b;
+}
+
+bool is_cycle(graph * g)
+{
+    size_t sz = g->n_vtex * sizeof(vtex *);
+    vtex ** parent = (vtex **)malloc(sz);
+    memset(parent, 0, sz);
+    for(int i = 0; i < g->n_edge; ++i)
+    {
+        vtex * a = find(parent, g->e_ary[i].src);
+        vtex * b = find(parent, g->e_ary[i].dest);
+        if(a == b)
+            return true;
+        uni(parent, a, b);
+    }
+    return false;
+}
+
+int main()
+{
+    int n_vtex = 3, n_edge = 3; /* also modify n_edge if more or less add_edge() calls */
+    graph * g = malloc_graph(n_vtex, n_edge);
+    vtex vtex_data[] = {{0}, {1}, {2}};
+    add_edge(g, vtex_data, vtex_data + 1);
+    add_edge(g, vtex_data, vtex_data + 2);
+    add_edge(g, vtex_data + 1, vtex_data + 2);
+    printf("is_cycle = %d\n", is_cycle(g));
+}
