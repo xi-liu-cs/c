b22fe803e6cf79321d98fa4f54fb0e3e0d7cf78a
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-08 10:45:18 -0400

    Create heap sort.c

---
+++ algo/graph/heap sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#define parent(i) ((i - 1) >> 1) /* floor((i - 1) / 2) */
+#define left(i) ((i << 1) + 1) /* 2i + 1 */  
+#define right(i) ((i << 1) + 2) /* 2i + 2 */ 
+
+typedef struct vtex
+{
+    int vtex_id;
+}vtex;
+
+void swap(vtex ** a , int i, int j)
+{
+    vtex * t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+}
+
+void min_heapify(vtex ** a, int i, int n)
+{
+    int l = left(i), r = right(i), min_i = i;
+    if(l < n && a[l]->vtex_id < a[min_i]->vtex_id)
+        min_i = l;
+    if(r < n && a[r]->vtex_id < a[min_i]->vtex_id)
+        min_i = r;
+    if(min_i != i)
+    {
+        swap(a, i, min_i);
+        min_heapify(a, min_i, n);
+    }
+}
+ 
+void build_min_heap(vtex ** a, int n)
+{
+    for(int i = parent(n - 1); i >= 0; --i)
+        min_heapify(a, i, n);
+}
+
+void heap_sort(vtex ** a, int n)
+{
+    build_min_heap(a, n);
+    for(int i = n - 1; i >= 1; --i)
+    {
+        swap(a, 0, i);
+        min_heapify(a, 0, i);
+    }
+}
+ 
+void print(vtex ** a, int n)
+{
+    for(int i = 0; i < n; ++i)
+        printf("%d ", a[i]->vtex_id);
+    printf("\n");
+}
+
+int main()
+{
+    vtex data[] = {{8}, {4}, {8}, {6}, {1}, {3}, {1}};
+    vtex * a[] = {data, data + 1, data + 2, data + 3, data + 4, data + 5, data + 6};
+    int n = sizeof(a) / sizeof(*a);
+    print(a, n);
+    heap_sort(a, n);
+    print(a, n);
+}
