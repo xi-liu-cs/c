1270ddf3a908f74637c2e9c7f33f39e421967cb4
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-17 18:01:36 -0400

    Update 17.cc

--- lc/17.cc
+++ lc/17.cc
@@ -1,30 +1,36 @@
+map<char, string> phone_map
+{
+    {'2', "abc"}, 
+    {'3', "def"},
+    {'4', "ghi"},
+    {'5', "jkl"}, 
+    {'6', "mno"},
+    {'7', "pqrs"},
+    {'8', "tuv"},
+    {'9', "wxyz"},
+};
+vector<string> comb;
+string str;
+string digit;
+
+void back_track(int i)
+{
+    if(i == digit.size())
+        comb.push_back(str);
+    string str_in_map = phone_map[digit[i]];
+    for(int j = 0; j < str_in_map.size(); ++j)
+    {
+        str += str_in_map[j];
+        back_track(i + 1);
+        str.erase(str.end() - 1);
+    }
+}
+
 vector<string> letterCombinations(string digits) 
 {
-    vector<string> comb;
+    digit = digits;
     if(!digits.size())
         return comb;
-    comb.push_back("");
-    map<char, string> phone_map
-    {
-        {'2', "abc"}, 
-        {'3', "def"},
-        {'4', "ghi"},
-        {'5', "jkl"}, 
-        {'6', "mno"},
-        {'7', "pqrs"},
-        {'8', "tuv"},
-        {'9', "wxyz"},
-    };
-    for(int i = 0; i < digits.size(); ++i)
-    {
-        string str_in_map = phone_map[digits[i]];
-        while(comb.back().size() == i)
-        {
-            string last = comb.back();
-            comb.pop_back();
-            for(int j = 0; j < str_in_map.size(); ++j)
-                comb.push_back(last + str_in_map[j]); 
-        }
-    }
+    back_track(0);
     return comb;
 }
