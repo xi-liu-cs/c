c9f1a8ef22ebe9cf2d1bf3ab15143fb25f20a3a2
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-17 18:58:25 -0400

    Update 17.c

--- lc/17.c
+++ lc/17.c
@@ -1,17 +1,36 @@
-char * map[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-char ** comb;
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+char * phone_map[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+char ** comb; int comb_sz;
+char * str; int str_sz;
+char * digit;
+
+void back_track(int i)
+{
+    if(i == strlen(digit))
+    {
+        size_t sz = (str_sz + 1) * sizeof(char);
+        char * buf = (char *)malloc(sz);
+        memcpy(buf, str, sz);
+        comb[comb_sz++] = buf;
+        return;
+    }
+    else
+    {
+        char * str_in_map = phone_map[i];
+        for(int j = 0; j < str_sz; ++j)
+        {
+            str[str_sz++] = str_in_map[j];
+            back_track(i + 1);
+            --str_sz;
+        }
+    }
+}
 
 char ** letterCombinations(char * digits, int * return_size)
 {
-    int n = strlen(digits);
-    if(!n)
-    {
-        *return_size = 0;
-        return comb;
-    }
-    comb = (char **)malloc();
-    for(int i = 0; i < n; ++i)
-    {
-        
-    }
+    digit = digits;
+    
 }
