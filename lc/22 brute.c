3db9a64f0b434ecf8478283e6ab7e49b93c7b2e9
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-21 23:57:53 -0400

    Update 22 brute.c

--- lc/22 brute.c
+++ lc/22 brute.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-char ** ret; int ret_sz = 0, ret_cap = 1;
-char * cur; int cur_sz = 0, cur_cap = 1;
-int _n;
+char ** ret; int ret_sz, ret_cap;
+char * cur; int cur_sz, cur_cap;
+int _n, ret_flag = 0, cur_flag = 1;
 
 bool valid()
 {
@@ -22,68 +22,80 @@ bool valid()
     return balance == 0;
 }
 
-void push_back(char ** cur, char c)
+void _realloc(int flag)
 {
-    if(cur_sz + 1 > cur_cap)
+    if(flag == ret_flag)
     {
-        cur_cap *= 2;
-        *cur = (char *)realloc(*cur, cur_cap * sizeof(char));
+        if(ret_sz + 1 > ret_cap)
+        {
+            ret_cap *= 2;   
+            ret = (char **)realloc(ret, ret_cap * sizeof(char *));
+        }
     }
-    if(*cur)
+    else if(flag == cur_flag)
     {
-        (*cur)[cur_sz - 1] = c;
-        (*cur)[cur_sz++] = '\0';   
+        if(cur_sz + 1 > cur_cap)
+        {
+            cur_cap *= 2;   
+            cur = (char *)realloc(cur, cur_cap * sizeof(char));
+        }
     }
 }
 
-void pop_back(char ** cur)
+void push_back(char c)
 {
-    if(cur_sz - 2 >= 0)
-        (*cur)[cur_sz - 2] = '\0';
+    _realloc(cur_flag);
+    cur[cur_sz - 1] = c; 
+    cur[cur_sz++] = '\0';
+}
+
+void pop_back()
+{
     --cur_sz;
+    if(cur_sz - 1 >= 0)
+        cur[cur_sz - 1] = '\0';
 }
 
 void generate()
 {
-    if(cur_sz - 1 == 2 * _n) /* since cur_sz includes '\0' */
+    if(cur_sz - 1 == 2 * _n)
     {
         if(valid())
         {
-            if(ret_sz + 1 > ret_cap)
-            {
-                ret_cap *= 2;
-                ret = (char **)realloc(ret, ret_cap * sizeof(char *));
-            }
-            char * buf = (char *)malloc(cur_sz * sizeof(char));
-            memcpy(buf, cur, cur_sz * sizeof(char));
-            ret[ret_sz++] = buf; 
+            _realloc(ret_flag);
+            size_t sz = cur_sz * sizeof(char);
+            char * buf = (char *)malloc(sz);
+            memcpy(buf, cur, sz);
+            ret[ret_sz++] = buf;
         }
         return;
     }
-    push_back(&cur, '(');
+    push_back('(');
     generate();
-    pop_back(&cur);
-    push_back(&cur, ')');
+    pop_back();
+    push_back(')');
     generate();
-    pop_back(&cur);
+    pop_back();
 }
 
 char ** generateParenthesis(int n, int * return_size)
 {
     _n = n;
+    ret_sz = cur_sz = 0;
+    ret_cap = cur_cap = 1;
     ret = (char **)malloc(ret_cap * sizeof(char *));
     cur = (char *)malloc(cur_cap * sizeof(char));
     *cur = '\0';
     cur_sz = 1;
     generate();
     *return_size = ret_sz;
-    return ret;
+    return ret; 
 }
 
 int main()
 {
     int retsz;
-    char ** ret = generateParenthesis(3, &retsz);
+    char ** ret = generateParenthesis(8, &retsz);
     for(int i = 0; i < retsz; ++i)
         printf("i = %d, %s\n", i, ret[i]);
 }
