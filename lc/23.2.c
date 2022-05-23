e4fc4ece3ed0dcf28951fe3b86e5104b69e2060b
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-23 12:30:09 -0400

    Create 23.2.c

---
+++ lc/23.2.c
@@ -0,0 +1,38 @@
+typedef struct ListNode node;
+
+node * merge_two_lists(node * a, node * b)
+{
+    if(!a || !b) return a ? a : b;
+    node head, * tail = &head, * a_ptr = a, * b_ptr = b;
+    while(a_ptr && b_ptr)
+    {
+        if(a_ptr->val < b_ptr->val)
+        {
+            tail->next = a_ptr;
+            a_ptr = a_ptr->next;
+        }
+        else
+        {
+            tail->next = b_ptr;
+            b_ptr = b_ptr->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = a_ptr ? a_ptr : b_ptr;
+    return head.next;
+}
+
+node * merge(node ** lists, int l, int r)
+{
+    if(l == r)
+        return lists[l];
+    if(l > r)
+        return 0;
+    int mid = l + ((r - l) >> 1); 
+    return merge_two_lists(merge(lists, l, mid), merge(lists, mid + 1, r));
+}
+
+node * mergeKLists(node ** lists, int lists_size)
+{
+    return merge(lists, 0, lists_size - 1);
+}
