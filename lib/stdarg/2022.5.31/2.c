1e9ecbc5ab12435672686d07f18e57c453151e7f
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-05-31 20:16:09 -0400

    Update 2.c

--- lib/stdarg/2022.5.31/2.c
+++ lib/stdarg/2022.5.31/2.c
@@ -12,8 +12,39 @@ int sum(int n, ...)
     return sum;
 }
 
+void print(char * fmt, ...)
+{
+    va_list ap; /* arg ptr */
+    int d;
+    char c, * s;
+    va_start(ap, fmt);
+    while(*fmt)
+    {
+        switch(*fmt++)
+        {
+            case 'c':
+                c = (char)va_arg(ap, int);
+                printf("char %c\n", c);
+                break;
+            case 'd':
+                d = va_arg(ap, int);
+                printf("int %d\n", d);
+                break;
+            case 's':
+                s = va_arg(ap, char *);
+                printf("string %s\n", s);
+                break;
+        }
+    }
+    va_end(ap);
+}
+
 int main()
 {
     int n = 3;
-    printf("%d\n", sum(n, 1, 2, 3));   
+    print("%d", sum(n, 1, 2, 3));   
+    char c = 'a';
+    print("%c", c);
+    char * s = "ab";
+    print("%s", s);
 }
