a9454ef8d04dac7596a24f5cfad59de59c9ee1ff
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-06-02 00:20:38 -0400

    Create 50.c

---
+++ lc/50.c
@@ -0,0 +1,17 @@
+typedef long long _long;
+
+double qmult(double x, _long n)
+{
+    if(!n)
+        return 1.0;
+    if(!(n % 2))
+        return qmult(x * x, n / 2);
+    else
+        return x * qmult(x * x, (n - 1) / 2);
+}
+
+double myPow(double x, int n)
+{
+    _long _n = n;
+    return n >= 0 ? qmult(x, n) : 1.0 / qmult(x, -_n);
+}
