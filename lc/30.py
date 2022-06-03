797f0d9b8fb163fdf6fecf324b6434b219d2e235
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-06-03 19:32:06 -0400

    Create 30.py

---
+++ lc/30.py
@@ -0,0 +1,17 @@
+def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    if not s or not words:
+        return []
+    from collections import Counter
+    word_len = len(words[0])
+    all_len = len(words) * word_len # all words len
+    s_len = len(s)
+    words = Counter(words)
+    ret = []
+    for i in range(0, s_len - all_len + 1):
+        tmp = s[i : i + all_len]
+        tmp_c = []
+        for j in range(0, all_len, word_len):
+            tmp_c.append(tmp[j : j + word_len])
+        if Counter(tmp_c) == words:
+            ret.append(i)
+    return ret
