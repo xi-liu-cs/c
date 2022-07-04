5d419dba445fcd143e1daac2f634357a9879ce4d
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-04 13:51:41 -0400

    Update 38.c

--- lc/38.c
+++ lc/38.c
@@ -3,11 +3,10 @@ char * countAndSay(int n)
     if(n == 1)
         return "1";
     char * pre = countAndSay(n - 1);
-    int cnt = 0;
     char cur = *pre;
     int pre_sz = strlen(pre);
     char * res = (char *)malloc((2 * pre_sz + 1) * sizeof(char));
-    int res_i = 0;
+    int res_i = 0, cnt = 0;
     for(int i = 0; i < pre_sz + 1; ++i)
     {
         if(pre[i] == cur)
