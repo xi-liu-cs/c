4fcc037cfe339127f8e393d51b3cd9678989f15d
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-07 20:21:52 -0400

    Create ca.c

---
+++ a/2022.7.7/ca.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+ 
+char map[] = "___#_##_";
+ 
+#define idx(i) (cell[i] != '_')
+int evolve(char * cell, char * buf, int n)
+{
+	int i, dif = 0;
+	for(i = 0; i < n; ++i) 
+	{/* use left, self, right as binary number bits for map index */
+		buf[i] = map[(idx(i - 1) << 2) + (idx(i) << 1) + idx(i + 1)];
+		dif += (buf[i] != cell[i]);
+	}
+	strcpy(cell, buf);
+	return dif;
+}
+ 
+int main()
+{
+	char c[] = "_###_##_#_#_#_#__#__\n",
+	b[] = "____________________\n";
+	int n = sizeof(c) / sizeof(*c);
+	do { printf("%s", c + 1); } while (evolve(c + 1, b + 1, n - 3));
+}
