5d7a3da8f62686ec41cfc875ca0fde5910aa6e47
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-07 23:19:38 -0400

    Create ca2.c

---
+++ a/2022.7.7/ca2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef unsigned long long ull;
+#define n 64
+#define b(x) (1ull << (x))
+#define rot_left(a, i, n) (a << i | a >> (n - i))
+#define print(i, n)\
+{\
+	for (i = n; --i; i)\
+	    putchar(st & b(i) ? '#' : '.');\
+	putchar('\n');\    
+}
+ 
+void evolve(ull state, int rule)
+{
+	int i;
+	ull st;
+ 
+	printf("rule %d:\n", rule);
+	do 
+	{
+		st = state;
+		for(i = n; --i; i) 
+		    putchar(st & b(i) ? '#' : '.');
+		putchar('\n');
+ 
+		for(state = i = 0; i < n; ++i)
+			if(rule & b(7 & rot_left(st, i, n)))
+				state |= b(i);
+	}while (st != state);
+}
+ 
+int main(int argc, char **argv)
+{
+	evolve(b(n / 2), 90);
+	evolve(b(n / 4) | b(n - n / 4), 30);
+}
