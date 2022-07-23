1d83184b864e5fdcf149f95073e5ba12e1d39a09
Xi Liu <66986728+xi-liu-cs@users.noreply.github.com>
2022-07-23 15:32:01 -0400

    Create vector.c

---
+++ container/vector.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define vector_struct(type)\
+  typedef struct\
+  {\
+    type * a;\
+    size_t cap;\
+    size_t cur;\
+  }type##_vector; 
+
+#define vector_init(type)\
+  void type##_vector_init(type##_vector * v)\
+  {\
+    v->cap = 1;\
+    v->a = malloc(sizeof(type) * v->cap);\
+    v->cur = 0;\
+  }\
+
+#define vector_push(type)\
+  void type##_vector_push(type##_vector * v, type data)\
+  {\
+    if (v->cur == v->cap)\
+    {\
+      v->cap *= 2;\
+      v->a = realloc(v->a, sizeof(type) * v->cap);\
+    }\
+    v->a[v->cur++] = data;\
+  }
+
+#define vector(type)\
+  vector_struct(type);\
+  vector_init(type) vector_push(type)
+
+int main() 
+{
+  vector(int);
+  int_vector v;
+  int_vector_init(&v);
+  int_vector_push(&v, 0);
+  printf("%d\n", *v.a);
+}
